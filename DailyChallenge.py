01503f26ec1fda87ffbd53326861ce92ee8acf41
Fransia <fsinsfran@gmail.com>
2024-06-11 15:03:59 -0400

    Add files via upload

---
+++ DailyChallenge.py
@@ -0,0 +1,5 @@
+# Solicitar al usuario que ingrese una cadena
+cadena = input("Introduce una cadena de caracteres: ")
+cadena_invertida = cadena[::-1]
+# Mostrar la cadena invertida
+print("La cadena invertida es:", cadena_invertida)
