fbd3ac5c3f806997b10304d939592e5c6c450ba3
Fransia <fsinsfran@gmail.com>
2024-06-19 13:29:25 -0400

    Add files via upload

---
+++ DailyChallenge6.py
@@ -0,0 +1,7 @@
+# Conversión de Temperatura: Escribe un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit.
+
+choise = float(input("Ingrese los grados en Celsius; "))
+
+cambio = (choise * (9/5) + 32)
+
+print(f"{choise} a grados Fahrenheit es igual a: {cambio}")
